9f3501acf8e10140f12a22ad0acf6c4cfd819de2
Matija-Aleksic <123953139+Matija-Aleksic@users.noreply.github.com>
2023-11-15 19:42:53 +0100

    lyrics formatting fixed

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,38 +1,33 @@
 import os
 import requests
-import mutagen
-import pyperclip
+import music_tag
+import tkinter as tk
+from tkinter import filedialog, simpledialog, messagebox
 from mutagen.easyid3 import EasyID3
 from mutagen.flac import FLAC
 from mutagen.oggvorbis import OggVorbis
 from bs4 import BeautifulSoup
-import music_tag
-import tkinter as tk
-from tkinter import filedialog, simpledialog
+import pyperclip
 
-# Function to get metadata from audio files
 def get_metadata(filepath):
     try:
         _, ext = os.path.splitext(filepath)
-        if ext in [".mp3", ".flac", ".ogg"]:
+        if ext == ".mp3" or ext == ".flac" or ext == ".ogg":
             audio = EasyID3(filepath) if ext == ".mp3" else FLAC(filepath) if ext == ".flac" else OggVorbis(filepath)
             artist = audio.get("artist", [""])[0]
             title = audio.get("title", [""])[0]
             return {"artist": artist, "title": title}
         else:
-            # unsupported file format
             return None
     except NotImplementedError as e:
         print(f"Error processing file: {filepath}")
         print(f"Error details: {e}")
         return None
 
-# Function to get lyrics from Genius
 def get_lyrics(artist, title):
     base_url = "https://api.genius.com"
     search_url = "/search"
     headers = {"Authorization": "Bearer IZH7cNut75QpRsXf9Pyqnf-WBSCDmJKldwQjfvzLifboY6mIVRnBMiQIeJs21vQ6"}
-
     query = f"{artist} {title}"
     params = {"q": query}
 
@@ -45,27 +40,29 @@ def get_lyrics(artist, title):
             song_url = results[0]["result"]["url"]
             print("Genius URL:", song_url)
 
-            # Fetch lyrics from the Genius website
             lyrics_response = requests.get(song_url)
             lyrics_response.raise_for_status()
-
-            # Use BeautifulSoup to extract lyrics from HTML
+            
             soup = BeautifulSoup(lyrics_response.text, "html.parser")
-            lyrics = soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-1 kUgSbL").get_text()
+            lyrics_container = soup.find("div", class_="Lyrics__Container-sc-1ynbvzw-1 kUgSbL")
+
+            # Get the text content of the lyrics container
+            lyrics_text = lyrics_container.get_text(separator='\n')
 
             print("Lyrics found:")
-            print(lyrics)
+            print(lyrics_text)
 
-            # Format lyrics
-            formatted_lyrics = lyrics.replace('[', '\n[').replace(']', ']\n')
+            formatted_lyrics = lyrics_text.replace('[', '\n[')
+            formatted_lyrics = formatted_lyrics.replace(']', ']\n')
+
             return formatted_lyrics.strip()
+
 
     except requests.exceptions.RequestException as err:
         print("Something went wrong:", err)
 
     return None
 
-# Function to delete lyrics from audio files
 def delete_lyrics(filepath):
     try:
         f = music_tag.load_file(filepath)
@@ -76,16 +73,14 @@ def delete_lyrics(filepath):
     except Exception as e:
         print(f"Error deleting lyrics: {e}")
 
-# Function for custom dialog box
-def custom_dialog(title, prompt, buttons):
+def custom_dialog_yes_no(title, prompt):
     root = tk.Tk()
     root.withdraw()
 
-    result = simpledialog.askstring(title, prompt, buttons=buttons)
+    result = messagebox.askyesno(title, prompt)
 
     return result
 
-# Function to scan a folder and perform specified action
 def scan_folder(folder_path, action_choice):
     for dirpath, _, filenames in os.walk(folder_path):
         for filename in filenames:
@@ -112,9 +107,8 @@ def scan_folder(folder_path, action_choice):
                 else:
                     print("Invalid choice.")
 
-# Main script
-buttons = ["Find Lyrics", "Delete Lyrics"]
-answer = custom_dialog("Lyrics Action", "Do you want to find or delete lyrics for all songs in the folder?", buttons)
+# Ask user whether to find or delete lyrics for the entire folder
+answer = custom_dialog_yes_no("Lyrics Action", "Do you want to find the lyrics? (pressing no will delete them)")
 
 if answer:
     root = tk.Tk()
@@ -122,6 +116,7 @@ if answer:
     folder_path = filedialog.askdirectory()
 
     # Call the function to scan the folder with the chosen action
-    scan_folder(folder_path, answer)
+    action_choice = "Find Lyrics" if answer else "Delete Lyrics"
+    scan_folder(folder_path, action_choice)
 else:
-    print("Invalid choice.")
+    print("User chose not to proceed.")
